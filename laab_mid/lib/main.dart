8ed267b7328726bbf4d445ea7093ae8955e3c46e
muhammad-usmman <99991611+Syed-AbdulBasit@users.noreply.github.com>
2022-05-07 11:09:37 +0500

    splash screen

--- laab_mid/lib/main.dart
+++ laab_mid/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:laab_mid/splash_screen/frontpage.dart';
 void main() {
   runApp( MyApp());
 }
---
+++ laab_mid/lib/splash_screen/frontpage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:mid_task/home.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                home()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child:FlutterLogo(size:MediaQuery.of(context).size.height),
+
+    );
+  }
+}
+
+
