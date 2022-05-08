4315e71fb48b0c4b0e4690c38887fb2b0449e317
muhammad-usmman <99991611+Syed-AbdulBasit@users.noreply.github.com>
2022-05-08 23:50:47 +0500

    update

--- laab_mid/lib/About_us.dart
+++ laab_mid/lib/About_us.dart
@@ -5,6 +5,55 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        home: Scaffold(
+        appBar: AppBar(title: Text('Contact Us'),backgroundColor:   Colors.teal,),
+
+    backgroundColor:   Colors.teal,
+    body: Container(
+
+    color:   Colors.teal,
+    child: Center(
+    child: Column(
+
+    children: [
+    SizedBox(height: 100.0),
+    Row(children: [
+
+    Icon(Icons.person),
+    SizedBox(width: 20.0,),
+    Text('Hi, My name is Syed Abdul Basit'),
+    ],
+    ),
+    SizedBox(height: 45.0,),
+    Row(
+    children: [
+    Text('I am a full stack Developoer mainly working from COMSATS University vehari'),
+    ],
+    ),
+    SizedBox(height: 50.0,),
+    Row(
+    children: [
+
+    Icon(Icons.mail),
+    SizedBox(width: 20.0,),
+    Text('I have made this app for better tasbiah')
+
+
+
+    ],
+    ),
+    ],
+    ),
+    ),
+
+
+    ),
+
+        ),
+
+
+    );
   }
 }
+
